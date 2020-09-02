0d2a9dec7594b0c3dd8dea368aff48a4bb5def01
Isaac-the-Man <steven97102@gmail.com>
2020-09-02 16:14:10 -0400

    tutorial added.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,36 @@
     <b-container>
       <b-row>
         <b-col>
-          <h1>Youtube Comment Snippet Downloader</h1>
+          <h1><span class="rounded"><BIconCaretRightFill style="color:#FF0000"></BIconCaretRightFill>Youtube</span>
+            Comment Snippet Downloader
+          </h1>
           <hr>
+        </b-col>
+      </b-row>
+      <b-row class="mt-3">
+        <b-col md="6">
+          <h3>Step1: Query Comments</h3>
+          Copy the Video ID of any Youtube videos, and query up to at most 1800 comments. Sort by relevance or time by
+          your choice.
+        </b-col>
+        <b-col md="6">
+          <b-img fluid src="@/assets/video_id.gif"></b-img>
+        </b-col>
+      </b-row>
+      <b-row class="mt-3">
+        <b-col md="6">
+          <b-img fluid src="@/assets/style_download.gif"></b-img>
+        </b-col>
+        <b-col md="6">
+          <h3>Step2: Style & Download</h3>
+          Currently supported customization includes: dark theme, html links, show time, show edited, and wide comment.
+          Download styled comments as <code>.png</code> images.
         </b-col>
       </b-row>
       <b-row>
         <b-col>
-          <Scraper></Scraper>
+          <hr>
+          <Scraper class="mt-3"></Scraper>
         </b-col>
       </b-row>
       <b-row>
@@ -24,12 +47,15 @@
 <script>
 import Scraper from "@/components/Scraper";
 import AppFooter from "@/components/AppFooter";
+import {BIconCaretRightFill} from 'bootstrap-vue';
 
 export default {
   name: 'App',
   components: {
     Scraper,
-    AppFooter
+    AppFooter,
+    // BICon,
+    BIconCaretRightFill
   }
 }
 </script>
