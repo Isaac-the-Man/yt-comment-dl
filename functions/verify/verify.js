9844f10d99e0dc03c108772a57573d2978382123
Isaac-the-Man <steven97102@gmail.com>
2020-09-04 10:36:19 -0400

    post function fixed

--- functions/verify/verify.js
+++ functions/verify/verify.js
@@ -1,6 +1,23 @@
-exports.handler = function(event, context, callback) {
-        callback(null, {
+const axios = require('axios');
+
+exports.handler = async function (event) {
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
+    const secret = process.env.GOOGLE_RECAPTCHA_SECRET;
+    const body = JSON.parse(event.body);
+    try {
+        const api = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${body.token}`;
+        const res = await axios.post(api);
+        return {
             statusCode: 200,
-            body: 'Verify API: ' + process.env.GOOGLE_RECAPTCHA_SECRET
-        });
+            body: JSON.stringify(res.data)
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        }
+    }
 }
