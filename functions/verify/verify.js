4ec7274c6c856d6fb3ca71dc8f1f5383b51d8b18
Isaac-the-Man <steven97102@gmail.com>
2020-09-04 09:59:48 -0400

    function fix

--- functions/verify/verify.js
+++ functions/verify/verify.js
@@ -1,19 +1,6 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const axios = require('axios').default;
-
-async function verityRecaptcha(token) {
-  const api = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_SECRET}&response=${token}`;
-  return await axios.post(api);
+exports.handler = function(event, context, callback) {
+        callback(null, {
+            statusCode: 200,
+            body: 'Verify API: ' + process.env.GOOGLE_RECAPTCHA_SECRET
+        });
 }
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post('/verify', async (req, res) => {
-  const response = verityRecaptcha(req.body.token);
-  res.json(response);
-});
-
-module.exports.handler = serverless(app);
