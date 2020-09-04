56dfb5a8af2bfd317c0631da0efbe65243f5eb7a
Isaac-the-Man <steven97102@gmail.com>
2020-09-04 13:35:44 -0400

    minor function fix

--- functions/verify/verify.js
+++ functions/verify/verify.js
@@ -5,9 +5,8 @@ exports.handler = async function (event) {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
     const secret = process.env.GOOGLE_RECAPTCHA_SECRET;
-    const body = JSON.parse(event.body);
     try {
-        const api = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${body.token}`;
+        const api = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${event.body.token}`;
         const res = await axios.post(api);
         return {
             statusCode: 200,
